9df332e7674bda8564048dd3e14fdeab202a712a
Jean Rakotoarison <jd.rakotoarison@gmail.com>
2023-02-13 19:15:05 +0300

    update MainWrapper styling

--- shared/layouts/MainWrapper.tsx
+++ shared/layouts/MainWrapper.tsx
@@ -1,31 +1,29 @@
 import { Grid, Typography } from "@mui/material";
-import { BASE_COLOR } from "../constants/colors";
 import { Outlet } from "react-router-dom";
+import useMainWrapperStyles from "../hooks/useMainWrapperStyles";
 
 const MainWrapper: React.FC = () => {
-    return (
-        <Grid container margin="auto" padding={1} maxWidth={1600}>
-            <Grid container>
-                <Grid item xs={6}>
-                    <Typography fontSize={30} color={BASE_COLOR.mainPrimary}>
-                        <strong>App logo</strong>
-                    </Typography>
-                </Grid>
+  const styles = useMainWrapperStyles();
 
-                <Grid item xs={6} display="flex" alignItems="center" justifyContent="flex-end">
-                    Menu
-                </Grid>
-            </Grid>
-
-            <Grid item xs={12}>
-                <Outlet />
-            </Grid>
-
-            <Grid item xs={12} textAlign="center">
-                <Typography>App name - {new Date().getFullYear()}</Typography>
-            </Grid>
+  return (
+    <Grid container sx={styles.MainWrapper}>
+      <Grid container>
+        <Grid item xs={6}>
+          <Typography sx={styles.MainWrapper.logoContainer}>
+            <strong>App logo</strong>
+          </Typography>
         </Grid>
-    );
+      </Grid>
+
+      <Grid item xs={12}>
+        <Outlet />
+      </Grid>
+
+      <Grid item xs={12} textAlign="center">
+        <Typography>App name - {new Date().getFullYear()}</Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default MainWrapper;
