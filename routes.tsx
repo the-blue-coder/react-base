e1e446dcb427351209c53eb2044c2ea9017ea1e0
Jean Rakotoarison <jd.rakotoarison@gmail.com>
2023-07-11 10:59:22 +0300

    VEMRouter to AppRouter

--- routes.tsx
+++ routes.tsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/containers/Home";
 import MainWrapper from "./shared/layouts/MainWrapper";
 
-const VEMRouter: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MainWrapper />}>
-                    <Route index element={<Home />}></Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainWrapper />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
-export default VEMRouter;
+export default AppRouter;
--- shared/constants/appDetails.ts
+++ shared/constants/appDetails.ts
@@ -1,1 +1,1 @@
-export const APP_NAME = "Vehicle Expense Manager";
+export const APP_NAME = "App name";
