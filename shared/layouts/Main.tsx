934675974ca10ea2f30bcc7cf31263763ab5389f
Jean Rakotoarison <jd.rakotoarison@gmail.com>
2023-07-12 21:34:19 +0300

    outlet er

--- shared/layouts/Main.tsx
+++ shared/layouts/Main.tsx
@@ -1,27 +1,20 @@
-import { Container, Row, Col } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
 import "../../assets/scss/shared/layouts/Main.scss";
 
 const Main: React.FC = () => {
-    return (
-        <div id="main-layout">
-            <Header />
+  return (
+    <div id="main-layout">
+      <Header />
 
-            <section id="content">
-                <Container fluid>
-                    <Row>
-                        <Col xs={12}>
-                            <Outlet />
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
+      <section id="content">
+        <Outlet />
+      </section>
 
-            <Footer />
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
