7c150bc04094c9f2126e403aee3c64fe35dd567a
Jean R. <jd.rakotoarison@gmail.com>
2023-08-29 13:20:22 +0300

    leftmenu

--- src-dashboard/shared/common/LeftMenu.tsx
+++ src-dashboard/shared/common/LeftMenu.tsx
@@ -1,5 +1,5 @@
 import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { routes } from "routes";
 import useAppRoutes from "shared/hooks/useAppRoutes";
 
@@ -7,12 +7,13 @@ const LeftMenu: React.FC = () => {
     const menus = [routes.overview, routes.groups, routes.posts, routes.products, routes.tasks];
     const { getRoutePath } = useAppRoutes();
     const navigate = useNavigate();
+    const location = useLocation();
 
     return (
         <List>
             {menus.map((menu) => (
                 <ListItem key={menu.path} disablePadding>
-                    <ListItemButton onClick={() => navigate(getRoutePath(menu))}>
+                    <ListItemButton onClick={() => navigate(getRoutePath(menu))} selected={`${menu.path}` === location.pathname}>
                         <ListItemText primary={menu.title} />
                     </ListItemButton>
                 </ListItem>
