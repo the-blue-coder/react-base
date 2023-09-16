63f1bd9751084dc22d42e0846112af4f7c2dd459
Jean R. <jd.rakotoarison@gmail.com>
2023-09-16 23:25:16 +0300

    updated left menu styles

--- src-dashboard/shared/styles/common/useLeftMenuStyles.ts
+++ src-dashboard/shared/styles/common/useLeftMenuStyles.ts
@@ -3,6 +3,10 @@ import { BASE_COLOR } from "shared/constants/colors.constant";
 const useLeftMenuStyles = () => {
     return {
         leftMenu: {
+            "& .MuiListItemButton-root": {
+                transition: "initial",
+                "-webkit-transition": "initial",
+            },
             "& .MuiListItemButton-root.Mui-selected, & .MuiListItemButton-root.Mui-selected:hover, & .MuiListItemButton-root:hover": {
                 backgroundColor: BASE_COLOR.primary,
                 color: "#ffffff",
