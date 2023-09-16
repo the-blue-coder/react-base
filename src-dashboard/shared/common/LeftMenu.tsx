3ca60dfd56b2a3e578fea6f6d58171597bd3f4bd
Jean R. <jd.rakotoarison@gmail.com>
2023-09-16 23:22:08 +0300

    updated Left Menu

--- src-dashboard/shared/common/LeftMenu.tsx
+++ src-dashboard/shared/common/LeftMenu.tsx
@@ -2,15 +2,17 @@ import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
 import { routes } from "routes";
 import useAppRoutes from "shared/hooks/useAppRoutes";
+import useLeftMenuStyles from "shared/styles/common/useLeftMenuStyles";
 
 const LeftMenu: React.FC = () => {
     const menus = [routes.overview, routes.groups, routes.posts, routes.products, routes.tasks];
     const { getRoutePath } = useAppRoutes();
     const navigate = useNavigate();
     const location = useLocation();
+    const styles = useLeftMenuStyles();
 
     return (
-        <List>
+        <List sx={styles.leftMenu}>
             {menus.map((menu) => (
                 <ListItem key={menu.path} disablePadding>
                     <ListItemButton onClick={() => navigate(getRoutePath(menu))} selected={`${menu.path}` === location.pathname}>
---
+++ src-dashboard/shared/styles/common/useLeftMenuStyles.ts
@@ -0,0 +1,14 @@
+import { BASE_COLOR } from "shared/constants/colors.constant";
+
+const useLeftMenuStyles = () => {
+    return {
+        leftMenu: {
+            "& .MuiListItemButton-root.Mui-selected, & .MuiListItemButton-root.Mui-selected:hover, & .MuiListItemButton-root:hover": {
+                backgroundColor: BASE_COLOR.primary,
+                color: "#ffffff",
+            },
+        },
+    };
+};
+
+export default useLeftMenuStyles;
