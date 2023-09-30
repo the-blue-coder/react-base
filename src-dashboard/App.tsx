6b5a6e64f310ff9d96cc244552beb4d06b335260
Jean R. <jd.rakotoarison@gmail.com>
2023-09-30 22:24:15 +0300

    updated index and App

--- src-dashboard/App.tsx
+++ src-dashboard/App.tsx
@@ -1,35 +1,15 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RecoilRoot } from "recoil";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { createTheme, Theme } from "@mui/material/styles";
-import { BASE_COLOR } from "shared/constants/colors.constant";
 import SnackbarList from "shared/components/snackbar/SnackbarList";
 import AppRouter from "shared/components/routes/AppRouter";
-
-const theme: Theme = createTheme({
-    typography: {
-        fontSize: 13,
-    },
-    palette: {
-        primary: {
-            main: BASE_COLOR.primary,
-        },
-    },
-});
-
-const queryClient = new QueryClient();
+import { Box, CssBaseline } from "@mui/material";
 
 const App: React.FC = () => {
     return (
-        <RecoilRoot>
-            <QueryClientProvider client={queryClient}>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <SnackbarList />
-                    <AppRouter />
-                </ThemeProvider>
-            </QueryClientProvider>
-        </RecoilRoot>
+        <Box>
+            {/* Needs implementation of page loading {pageIsLoading && <FullPageLoader />} */}
+            <CssBaseline />
+            <SnackbarList />
+            <AppRouter />
+        </Box>
     );
 };
 
--- src-dashboard/index.tsx
+++ src-dashboard/index.tsx
@@ -2,11 +2,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/scss/custom-styles.scss";
 import "./assets/scss/overrides.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "@mui/material";
+import { createTheme, Theme } from "@mui/material/styles";
+import { BASE_COLOR } from "shared/constants/colors.constant";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
+const theme: Theme = createTheme({
+    typography: {
+        fontSize: 13,
+    },
+    palette: {
+        primary: {
+            main: BASE_COLOR.primary,
+        },
+    },
+});
+
+const queryClient = new QueryClient();
+
 root.render(
-    <>
-        <App />
-    </>
+    <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </QueryClientProvider>
+    </RecoilRoot>
 );
