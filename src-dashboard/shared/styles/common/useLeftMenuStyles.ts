129d5228476f11f8d97a9ee11148895695b4de38
Jean R. <jd.rakotoarison@gmail.com>
2023-10-13 09:58:27 +0300

    updated useLeftMenuStyles

--- src-dashboard/shared/styles/common/useLeftMenuStyles.ts
+++ src-dashboard/shared/styles/common/useLeftMenuStyles.ts
@@ -7,6 +7,7 @@ const useLeftMenuStyles = () => {
                 transition: "initial",
                 WebkitTransition: "initial",
             },
+
             "& .MuiListItemButton-root.Mui-selected, & .MuiListItemButton-root.Mui-selected:hover, & .MuiListItemButton-root:hover": {
                 backgroundColor: BASE_COLOR.primary,
                 color: "#ffffff",
