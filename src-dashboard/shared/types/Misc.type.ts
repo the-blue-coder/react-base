16156933e8436514d6521d464972d0fd22b2506e
Jean R. <jd.rakotoarison@gmail.com>
2023-10-14 06:02:49 +0300

    updated misc type

--- src-dashboard/shared/types/Misc.type.ts
+++ src-dashboard/shared/types/Misc.type.ts
@@ -1,3 +1,11 @@
 export type DynamicObject = { [key: string]: DynamicObject | string | number | boolean };
 
+export type KeyValueObject = { key: string; value: string };
+
 export type ColorsType = "inherit" | "error" | "success" | "primary" | "secondary" | "info" | "warning";
+
+export type TimestampDataType = {
+    date: string;
+    timezone_type: number;
+    timezone: string;
+};
