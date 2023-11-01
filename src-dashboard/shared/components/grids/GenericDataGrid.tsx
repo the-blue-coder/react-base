c33648893ce0297c7b0dde0d1c1040671830f9ae
Jean R. <jd.rakotoarison@gmail.com>
2023-11-01 11:16:10 +0300

    update grid

--- src-dashboard/shared/components/grids/GenericDataGrid.tsx
+++ src-dashboard/shared/components/grids/GenericDataGrid.tsx
@@ -1,69 +1,56 @@
-import { useRef, forwardRef, useImperativeHandle } from "react";
+import { GenericDataGridProps } from "shared/types/Grids.type";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box } from "@mui/material";
 import useGenericDataGridStyles from "shared/styles/components/grids/useGenericDataGridStyles";
+import { useRef } from "react";
 import { defaultDataGridPageSize } from "shared/constants/MuiDataGrid.constant";
-import { GenericDataGridProps } from "shared/types/Grids.type";
-import { RefType } from "shared/types/Misc.type";
 
-const GenericDataGrid = forwardRef<RefType, GenericDataGridProps>(
-    (
-        {
-            columns,
-            rows,
-            pageSize = defaultDataGridPageSize,
-            checkboxSelection = true,
-            disableRowSelectionOnClick = false,
-            disableColumnMenu = false,
-            isLoading = false,
-            onCellDoubleClick,
-        },
-        ref
-    ) => {
-        const styles = useGenericDataGridStyles();
+const GenericDataGrid: React.FC<GenericDataGridProps> = ({
+    apiRef,
+    columns,
+    rows,
+    pageSize = defaultDataGridPageSize,
+    checkboxSelection = true,
+    disableRowSelectionOnClick = false,
+    disableColumnMenu = false,
+    isLoading = false,
+    onCellDoubleClick,
+}) => {
+    const styles = useGenericDataGridStyles();
 
-        const gridRef = useRef<HTMLDivElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
 
-        const handlePageChangeScrollToGridTop = () => {
-            if (gridRef?.current) {
-                gridRef.current.scrollIntoView();
-            }
-        };
+    const handlePageChangeScrollToGridTop = () => {
+        if (ref?.current) {
+            ref.current.scrollIntoView();
+        }
+    };
 
-        // Use useImperativeHandle to expose a function through the ref
-        useImperativeHandle(ref, () => ({
-            scrollIntoView: () => {
-                if (gridRef?.current) {
-                    gridRef.current.scrollIntoView();
-                }
-            },
-        }));
-
-        return (
-            <Box sx={styles.genericDataGrid}>
-                <DataGrid
-                    ref={gridRef}
-                    rows={rows}
-                    columns={columns}
-                    initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize,
-                            },
+    return (
+        <Box sx={styles.genericDataGrid}>
+            <DataGrid
+                apiRef={apiRef}
+                ref={ref}
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize,
                         },
-                    }}
-                    autoHeight
-                    loading={isLoading}
-                    pageSizeOptions={[pageSize]}
-                    checkboxSelection={checkboxSelection}
-                    disableRowSelectionOnClick={disableRowSelectionOnClick}
-                    disableColumnMenu={disableColumnMenu}
-                    onCellDoubleClick={onCellDoubleClick}
-                    onPaginationModelChange={handlePageChangeScrollToGridTop}
-                />
-            </Box>
-        );
-    }
-);
+                    },
+                }}
+                autoHeight
+                loading={isLoading}
+                pageSizeOptions={[pageSize]}
+                checkboxSelection={checkboxSelection}
+                disableRowSelectionOnClick={disableRowSelectionOnClick}
+                disableColumnMenu={disableColumnMenu}
+                onCellDoubleClick={onCellDoubleClick}
+                onPaginationModelChange={handlePageChangeScrollToGridTop}
+            />
+        </Box>
+    );
+};
 
 export default GenericDataGrid;
--- src/shared/types/Grids.type.ts
+++ src/shared/types/Grids.type.ts
@@ -1,6 +1,9 @@
 import { GridColDef, GridEventListener, GridValidRowModel } from "@mui/x-data-grid";
+import { GridApiCommunity } from "@mui/x-data-grid/internals";
+import { MutableRefObject } from "react";
 
 export type GenericDataGridProps = {
+    apiRef: MutableRefObject<GridApiCommunity>;
     columns: GridColDef[];
     rows: GridValidRowModel[];
     pageSize?: number;
