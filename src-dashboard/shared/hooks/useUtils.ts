03e61dac482b1b4e55fb92c8b7d0192ec988739b
Jean R. <jd.rakotoarison@gmail.com>
2023-11-04 15:35:10 +0300

    update useUtils

--- src-dashboard/shared/hooks/useUtils.ts
+++ src-dashboard/shared/hooks/useUtils.ts
@@ -1,11 +1,21 @@
+import { DEFAULT_CURRENCY } from "shared/constants/currency.constant";
+
 const useUtils = () => {
+    const formatNumber = (value: number, locale: string = "fr-FR") => {
+        return new Intl.NumberFormat(locale).format(value);
+    };
+
+    const formatNumberToCurrency = (value: number, currency?: string) => {
+        return `${formatNumber(value)} ${currency ?? DEFAULT_CURRENCY}`;
+    };
+
     const sortArrayAsc = (array: number[]) => {
         const sortedArray = [...array];
         sortedArray.sort((a, b) => a - b);
         return sortedArray;
     };
 
-    return { sortArrayAsc };
+    return { formatNumber, formatNumberToCurrency, sortArrayAsc };
 };
 
 export default useUtils;
