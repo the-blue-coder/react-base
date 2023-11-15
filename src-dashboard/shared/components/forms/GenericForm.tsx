bc125b3b71ef7cf6d3fa754e21074f02171df11e
Jean R. <jd.rakotoarison@gmail.com>
2023-11-15 07:59:08 +0300

    generic form

--- src-dashboard/shared/components/forms/GenericForm.tsx
+++ src-dashboard/shared/components/forms/GenericForm.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { forwardRef } from "react";
 import Form from "@rjsf/mui";
 import { GenericFormProps } from "../../types/Forms.type";
 import { IChangeEvent } from "@rjsf/core";
@@ -7,60 +8,54 @@ import { ErrorTransformer, RJSFSchema } from "@rjsf/utils";
 import { Box } from "@mui/material";
 import useGenericFormStyles from "shared/styles/components/forms/useGenericFormStyles";
 
-const GenericForm: React.FC<GenericFormProps> = ({
-    formData = {},
-    schema,
-    hideDefaultSubmitBtn = false,
-    children,
-    onFormChange,
-    onSingleFieldBlur,
-    onSubmit,
-    ...props
-}) => {
-    const styles = useGenericFormStyles(hideDefaultSubmitBtn);
+const GenericForm = forwardRef<any, GenericFormProps>(
+    ({ formData = {}, schema, hideDefaultSubmitBtn = false, children, onFormChange, onSingleFieldBlur, onSubmit, ...props }, ref) => {
+        const styles = useGenericFormStyles(hideDefaultSubmitBtn);
 
-    const transformErrors: ErrorTransformer<any, RJSFSchema, any> = (errors, uiSchema) => {
-        return errors.map((error) => {
-            const errorType = String(error.name);
-            const fieldProperty = String(error.property?.slice(1));
-            const customErrorMessage = uiSchema?.[fieldProperty]?.["ui:customErrorMessages"]?.[errorType];
-            const fieldName = error.stack.split("' ")[0]?.slice(1);
-            const updatedMessage = `${fieldName} ${error.message?.toLowerCase()}`;
+        const transformErrors: ErrorTransformer<any, RJSFSchema, any> = (errors, uiSchema) => {
+            return errors.map((error) => {
+                const errorType = String(error.name);
+                const fieldProperty = String(error.property?.slice(1));
+                const customErrorMessage = uiSchema?.[fieldProperty]?.["ui:customErrorMessages"]?.[errorType];
+                const fieldName = error.stack.split("' ")[0]?.slice(1);
+                const updatedMessage = `${fieldName} ${error.message?.toLowerCase()}`;
 
-            return { ...error, message: customErrorMessage || updatedMessage };
-        });
-    };
+                return { ...error, message: customErrorMessage || updatedMessage };
+            });
+        };
 
-    const handleChange = ({ formData: newFormData }: IChangeEvent<any>): void => {
-        onFormChange?.(newFormData);
-    };
+        const handleChange = ({ formData: newFormData }: IChangeEvent<any>): void => {
+            onFormChange?.(newFormData);
+        };
 
-    const handleBlur = (id: string, data: any): void => {
-        onSingleFieldBlur?.(id, data);
-    };
+        const handleBlur = (id: string, data: any): void => {
+            onSingleFieldBlur?.(id, data);
+        };
 
-    const handleSubmit = ({ formData: newFormData }: IChangeEvent<any>) => {
-        onSubmit?.(newFormData);
-    };
+        const handleSubmit = ({ formData: newFormData }: IChangeEvent<any>) => {
+            onSubmit?.(newFormData);
+        };
 
-    return (
-        <Box sx={styles.genericForm}>
-            <Form
-                {...props}
-                schema={schema.schema}
-                uiSchema={schema.uiSchema}
-                formData={formData}
-                validator={validator}
-                showErrorList={false}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                onSubmit={handleSubmit}
-                transformErrors={transformErrors}
-            >
-                {children}
-            </Form>
-        </Box>
-    );
-};
+        return (
+            <Box sx={styles.genericForm}>
+                <Form
+                    {...props}
+                    ref={ref}
+                    schema={schema.schema}
+                    uiSchema={schema.uiSchema}
+                    formData={formData}
+                    validator={validator}
+                    showErrorList={false}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    onSubmit={handleSubmit}
+                    transformErrors={transformErrors}
+                >
+                    {children}
+                </Form>
+            </Box>
+        );
+    }
+);
 
 export default GenericForm;
