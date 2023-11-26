5ee5cbfeea71dff34565f43b1c99fbfcee540c37
Jean R. <jd.rakotoarison@gmail.com>
2023-11-26 23:01:14 +0300

    update section styles

--- src-dashboard/shared/styles/components/texts/useSectionTitleStyles.ts
+++ src-dashboard/shared/styles/components/texts/useSectionTitleStyles.ts
@@ -3,10 +3,13 @@ import { BASE_COLOR } from "shared/constants/colors.constant";
 const useSectionTitleStyles = () => {
     return {
         sectionTitle: {
-            marginBottom: 1,
-            fontSize: 18,
-            fontWeight: "bold",
-            color: BASE_COLOR.primary,
+            marginBottom: 0,
+
+            "& h1": {
+                fontSize: 18,
+                fontWeight: "bold",
+                color: BASE_COLOR.primary,
+            },
         },
     };
 };
