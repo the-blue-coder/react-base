4bf45a61ca8211d2e0acff3c17869d58c6ca1439
Jean R. <jd.rakotoarison@gmail.com>
2023-12-26 11:54:08 +0300

    updated src-spa approuter

--- src-spa/generics/components/routes/AppRouter.tsx
+++ src-spa/generics/components/routes/AppRouter.tsx
@@ -1,28 +1,13 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "generics/components/errors/PageNotFound";
-import { defaultRoute, routes } from "routes";
-import useAppRoutes from "generics/hooks/useAppRoutes";
+import VideoEditor from "container/VideoEditor";
 
 const AppRouter: React.FC = () => {
-    const { getRoutePath } = useAppRoutes();
-
     return (
         <BrowserRouter>
             <Routes>
                 {/* Redirect / to overview */}
-                <Route path="/" element={<Navigate to={getRoutePath(defaultRoute)} />}></Route>
-
-                {Object.keys(routes).map((globalRouteKey) => {
-                    const routeElement = routes[globalRouteKey];
-
-                    return (
-                        <Route key={globalRouteKey} path={`/${globalRouteKey}`} element={routeElement.layout}>
-                            {Object.entries(routeElement.subRoutes).map(([key, routeItem]) => (
-                                <Route key={key} path={getRoutePath(routeItem)} element={routeItem.container}></Route>
-                            ))}
-                        </Route>
-                    );
-                })}
+                <Route path="/" element={<VideoEditor />}></Route>
 
                 {/* 404 page */}
                 <Route path="*" element={<PageNotFound />} />
