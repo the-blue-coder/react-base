2234b1513e6bb9b40cdeb91bdeeb4507dfbf1a5d
Jean R. <jd.rakotoarison@gmail.com>
2024-01-02 09:09:29 +0300

    generic submit button

---
+++ src/shared/components/buttons/GenericSubmitButton.tsx
@@ -0,0 +1,8 @@
+import { GenericButtonProps } from "shared/types/Buttons.type";
+import GenericButton from "./GenericButton";
+
+const GenericSubmitButton: React.FC<GenericButtonProps> = ({ children }) => {
+    return <GenericButton type="submit">{children ?? "Submit"}</GenericButton>;
+};
+
+export default GenericSubmitButton;
