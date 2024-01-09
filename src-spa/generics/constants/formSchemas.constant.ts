d9f39595aab99522307906bdab5bd2ce47aea271
Jean R. <jd.rakotoarison@gmail.com>
2024-01-09 13:39:28 +0300

    constants

---
+++ src-spa/generics/constants/formSchemas.constant.ts
@@ -0,0 +1,13 @@
+export const CustomSelectFieldBooleanUIOptions = {
+    hasClearButton: false,
+    options: [
+        {
+            id: 1,
+            value: "Yes",
+        },
+        {
+            id: 2,
+            value: "No",
+        },
+    ],
+};
